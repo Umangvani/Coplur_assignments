55e157d46c1dd88f8261703c260ffefd33e9ce3f
Umang Vani <119105216+Umangvani@users.noreply.github.com>
2025-01-15 01:58:58 +0530

    completed Assignment 2

---
+++ assignment2.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+class Notification(ABC):
+    @abstractmethod
+    def send(self, recipient, message):
+        pass
+
+class EmailNotification(Notification):
+    def send(self, recipient, message):
+        print(f"Sending Email to {recipient}: {message}")
+
+class SMSNotification(Notification):
+    def send(self, recipient, message):
+        print(f"Sending SMS to {recipient}: {message}")
+
+class PushNotification(Notification):
+    def send(self, recipient, message):
+        print(f"Sending Push Notification to {recipient}: {message}")
+
+
+def write_Notification(channel,recipient, message):
+    if(channel == "email"):
+        em = EmailNotification()
+        return em.send(recipient, message)
+    
+    if(channel == "sms"):
+        sm = SMSNotification()
+        return sm.send(recipient, message)
+    
+    if(channel == "push"):
+        pm = PushNotification()
+        return pm.send(recipient, message)
+    
+    if not channel:
+        print("Invalid notification channel.")
+    
+
+    
+if __name__ == "__main__":
+    print("\nNew Policy Announcement:")
+    write_Notification("email", "umangvani777@gmail.com" ,"A new policy has been implemented." )
+    
+    
+    print("\nEvent Reminder:")
+    write_Notification("sms", 9154306476 , "Don't forget the Coplur 2025 party tomorrow")
+    
+    print("\nSystem Alert:")
+    write_Notification("push", "samyak", "database drop till 6pm")
